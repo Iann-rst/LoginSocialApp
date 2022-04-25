d81809d342c463939795d4637f01dd396fd5fc84
Iann-rst <iannrst11@gmail.com>
2022-04-25 20:00:26 -0300

    Componente Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,1 +1,16 @@
 /* Fazer login no app com a conta do google */
+import React from 'react';
+
+import { Container } from './styles';
+import { Button } from '../Button';
+
+export function Form() {
+  return (
+    <Container>
+      <Button
+        icon="google"
+        title="Entrar com o Google"
+      />
+    </Container>
+  )
+}
